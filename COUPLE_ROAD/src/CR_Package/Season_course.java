8b0a6621b769fd174cc858da54840cc4478f7730
Paykey <wltjd5827@naver.com>
2023-11-29 13:15:54 +0900

    "Base"

---
+++ COUPLE_ROAD/src/CR_Package/Season_course.java
@@ -0,0 +1,15 @@
+package CR_Package;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Season_course extends JFrame implements ActionListener{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
